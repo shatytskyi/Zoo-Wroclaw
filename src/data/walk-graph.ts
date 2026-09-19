a9cfb18269ea47b2ab08e60c2004476f1054598f
Serhii <68147163+shatytskyi@users.noreply.github.com>
2026-09-19 17:39:51 +0200

    Load walking graph from compact JSON

--- src/data/walk-graph.ts
+++ src/data/walk-graph.ts
@@ -1,1 +1,5 @@
-PLACEHOLDER_USE_FILE
+import nodes from "./walk-nodes.json";
+import edges from "./walk-edges.json";
+
+export const WALK_NODES = nodes as [number, number][];
+export const WALK_EDGES = edges as [number, number][];
