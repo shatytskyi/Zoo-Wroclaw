a17cb94d2428eec81d69fefdc1a9e38273817353
Serhii <68147163+shatytskyi@users.noreply.github.com>
2026-09-19 17:44:24 +0200

    Add walking-graph node chunks

--- src/data/walk-graph.ts
+++ src/data/walk-graph.ts
@@ -1,5 +1,7 @@
-import nodes from "./walk-nodes.json";
+import n0 from "./walk-nodes-0.json";
+import n1 from "./walk-nodes-1.json";
+import n2 from "./walk-nodes-2.json";
 import edges from "./walk-edges.json";
 
-export const WALK_NODES = nodes as [number, number][];
+export const WALK_NODES = [...n0, ...n1, ...n2] as [number, number][];
 export const WALK_EDGES = edges as [number, number][];
